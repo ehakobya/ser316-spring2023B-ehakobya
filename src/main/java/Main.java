b415bf19b2a3ce1702dee63c99aeb046859b50fd
ehakobya <ehakobya@asu.edu>
2023-04-27 23:00:29 -0700

    Task 3.2 Fix Duplicate Code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,48 +2,33 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("**** GAME A ****");
-        GamePlay gamePlay = new GamePlay();
-        for (int round = 1; round < 9; round++) {
-            System.out.println("\tRound " + round);
-            if (gamePlay.player.health > 0) {
-                System.out.println("\t\tYou gained " + gamePlay.play()
-                        + " experience points during this round!!!!\n");
-            }
-            if (gamePlay.player.health <= 0) {
-                System.out.println("\t\tBut your player died. Better luck next time.\n\n");
-                break;
-            }
-        }
+        int numGames = 3;
+        char gameLetter = 'A';
+        GamePlay gamePlay;
 
-        System.out.println("**** GAME B ****");
-        GamePlay gamePlay1 = new GamePlay(new Wizard());
-        gamePlay1.player.health = 10;
-        for (int round = 1; round < 9; round++) {
-            System.out.println("\tRound " + round);
-            if (gamePlay1.player.health > 0) {
-                System.out.println("\t\tYou gained " + gamePlay1.play()
-                        + " experience points during this round!!!!\n");
+        for (int i = 0; i < numGames; i++) {
+            System.out.printf("**** GAME %c ****%n", gameLetter);
+            if (i == 0) {
+                gamePlay = new GamePlay();
+            } else if (i == 1) {
+                gamePlay = new GamePlay(new Wizard());
+                gamePlay.player.health = 10;
+            } else {
+                gamePlay = new GamePlay(new Barbarian(), new Wizard());
+                gamePlay.opponents.get(0).health = 5;
             }
-            if (gamePlay1.player.health <= 0) {
-                System.out.println("\t\tBut your player died. Better luck next time.\n\n");
-                break;
+            for (int round = 1; round < 9; round++) {
+                System.out.println("\tRound " + round);
+                if (gamePlay.player.health > 0) {
+                    System.out.println("\t\tYou gained " + gamePlay.play()
+                            + " experience points during this round!!!!\n");
+                }
+                if (gamePlay.player.health <= 0) {
+                    System.out.println("\t\tBut your player died. Better luck next time.\n\n");
+                    break;
+                }
             }
-        }
-
-        System.out.println("**** GAME C ****");
-        GamePlay gamePlay2 = new GamePlay(new Barbarian(), new Wizard());
-        gamePlay2.opponents.get(0).health = 5;
-        for (int round = 1; round < 9; round++) {
-            System.out.println("\tRound " + round);
-            if (gamePlay2.player.health > 0) {
-                System.out.println("\t\tYou gained " + gamePlay2.play()
-                        + " experience points during this round!!!!\n");
-            }
-            if (gamePlay2.player.health <= 0) {
-                System.out.println("\t\tBut your player died. Better luck next time.\n\n");
-                break;
-            }
+            gameLetter++;
         }
     }
 }
